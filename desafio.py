850580cdd17fcd4627c39284f4caa60e795db3c6
Patricia Lima Felismino <patriciafelismino12@gmail.com>
2025-09-30 18:12:33 -0300

    commit

---
+++ desafio.py
@@ -0,0 +1,15 @@
+#Classes e Objetos
+#Vamos Praticar : Para entendermos melhor o funcionamento e
+#escrita das classes e objetos em python.
+#1.Sistema de Gerenciamento de Livros Básico.
+#2.Objetivo: Criar uma classe simples e praticar a inicialização
+#3.O construtor (__init__) deve aceitar e armazenar titulo, autor e
+#ISBN (um número de identificação único).
+#4.Adicione um método detalhes() que retorna uma string formada
+#com todas as informações do livro.
+#5.Crie pelo menos dois objetos Livro diferentes e chame o método
+#detalhes() para cada um.
+#OBS! Após criar o algoritmo acima com tudo o que ele necessita,
+#pratique novamente criando uma nova classe com no mínimo dois
+#objetos e após isso, suba os dois algoritmos para um repositório no
+#github específico para classes e objetos.
